3d4a90f87b3aa16ed22563c8aae4beadbc853676
JonathanTonckensHU <jonathan.tonckens@student.hu.nl>
2019-05-22 14:01:09 +0200

    interface methods added

--- src/main/java/nl/hu/bep/group4/bifi/interfaces/FactuurLader.java
+++ src/main/java/nl/hu/bep/group4/bifi/interfaces/FactuurLader.java
@@ -1,5 +1,9 @@
 package nl.hu.bep.group4.bifi.interfaces;
 
+import java.util.List;
+
+import nl.hu.bep.group4.bifi.model.Factuur;
+
 public interface FactuurLader {
-	
+	public List<Factuur> getFacturenVoorMaand(int maandNummer);
 }
--- src/main/java/nl/hu/bep/group4/bifi/interfaces/IEFExporter.java
+++ src/main/java/nl/hu/bep/group4/bifi/interfaces/IEFExporter.java
@@ -1,5 +1,9 @@
 package nl.hu.bep.group4.bifi.interfaces;
 
-public interface IEFExporter {
+import java.util.List;
 
+import nl.hu.bep.group4.bifi.model.Factuur;
+
+public interface IEFExporter {
+	public void exportFacturen(List<Factuur> facturen);
 }
