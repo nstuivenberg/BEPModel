93dcf8082329ea12c3d7c7e5c197943bd2897975
Matthijs Osterhaus <matthijs@117pc253.sshunet.nl>
2019-05-30 14:47:13 +0200

    getters and setters added for unit and btwcode

--- src/main/java/nl/hu/bep/group4/bifi/model/FactuurRegel.java
+++ src/main/java/nl/hu/bep/group4/bifi/model/FactuurRegel.java
@@ -5,6 +5,9 @@ public class FactuurRegel {
     private String productNaam;
     private double aantal;
     private double totaalprijsExBTW;
+    private BTWcode btwCode;
+    private Unit unit;
+    
     public enum BTWcode {
         HOOG,
         LAAG,
@@ -15,37 +18,63 @@ public class FactuurRegel {
     }
     
     public FactuurRegel() {}
-    
-	public FactuurRegel(int productID, String productNaam, double aantal, double totaalprijsExBTW) {
-		super();
+
+	public FactuurRegel(int productID, String productNaam, double aantal, double totaalprijsExBTW, BTWcode btwCode,
+			Unit unit) {
 		this.productID = productID;
 		this.productNaam = productNaam;
 		this.aantal = aantal;
 		this.totaalprijsExBTW = totaalprijsExBTW;
+		this.btwCode = btwCode;
+		this.unit = unit;
 	}
+
 	public int getProductID() {
 		return productID;
 	}
+
 	public void setProductID(int productID) {
 		this.productID = productID;
 	}
+
 	public String getProductNaam() {
 		return productNaam;
 	}
+
 	public void setProductNaam(String productNaam) {
 		this.productNaam = productNaam;
 	}
+
 	public double getAantal() {
 		return aantal;
 	}
+
 	public void setAantal(double aantal) {
 		this.aantal = aantal;
 	}
+
 	public double getTotaalprijsExBTW() {
 		return totaalprijsExBTW;
 	}
+
 	public void setTotaalprijsExBTW(double totaalprijsExBTW) {
 		this.totaalprijsExBTW = totaalprijsExBTW;
 	}
-    
+
+	public BTWcode getBtwCode() {
+		return btwCode;
+	}
+
+	public void setBtwCode(BTWcode btwCode) {
+		this.btwCode = btwCode;
+	}
+
+	public Unit getUnit() {
+		return unit;
+	}
+
+	public void setUnit(Unit unit) {
+		this.unit = unit;
+	}
+
 }
