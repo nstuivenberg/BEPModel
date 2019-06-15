ab243b02e449932a6c0534f18f8834308ede5899
nick.stuivenberg <nick.stuivenberg@cgi.com>
2019-06-15 16:39:51 +0200

    PersoonTest af. Push voor coverage check.

--- src/test/java/nl/hu/bep/group4/bifi/model/FactuurTest.java
+++ src/test/java/nl/hu/bep/group4/bifi/model/FactuurTest.java
@@ -40,43 +40,6 @@ public class FactuurTest {
         legeFactuur.setKlant(klant);
         assertEquals(klant, legeFactuur.getKlant());
     }
-
-    /*
-    @Test
-    public void testDatumTijd() {
-        Instant dateTimeNow = Instant.now();
-        factuur.setDatumtijd(dateTimeNow.toString());
-        assertEquals(dateTimeNow.toString(), factuur.getDatumtijd());
-    }
-
-    @Test
-    public void testFactuurNummer() {
-        factuur.setFactuurNummer(666);
-        assertEquals(666, factuur.getFactuurNummer());
-    }
-
-    @Test
-    public void testFactuurRegels() {
-        List<FactuurRegel> factuurRegelList = new ArrayList<>();
-        factuur.setFactuurregels(factuurRegelList);
-        assertTrue(factuur.getFactuurregels().isEmpty());
-        assertEquals(factuurRegelList, factuur.getFactuurregels());
-    }
-
-    @Test
-    public void testOpmerking() {
-        factuur.setOpmerking("opmerking");
-        assertEquals("opmerking", factuur.getOpmerking());
-    }
-
-    @Test
-    public void testContactPersoon() {
-        Persoon persoon = new Persoon(1);
-        factuur.setContactPersoon(persoon);
-        assertEquals(persoon, factuur.getContactPersoon());
-    }
-
-*/
     @Test
     public void testToString() {
         setup();
--- src/test/java/nl/hu/bep/group4/bifi/model/PersoonTest.java
+++ src/test/java/nl/hu/bep/group4/bifi/model/PersoonTest.java
@@ -30,8 +30,61 @@ public class PersoonTest {
         assertEquals("0612345678", persoon.getTelefoon());
         assertEquals("0612345678", persoon.getFax());
         assertEquals(Persoon.Geslacht.VROUW, persoon.getGeslacht());
+    }
 
+    @Test
+    public void testSetId() {
+        createPersoon();
+        persoon.setId(666);
+        assertEquals(666, persoon.getId());
+    }
+    @Test
+    public void testSetVoornaam() {
+        createPersoon();
+        persoon.setVoornaam("Sjard");
+        assertEquals("Sjard", persoon.getVoornaam());
+    }
 
+    @Test
+    public void testSetAchternaam() {
+        createPersoon();
+        persoon.setAchternaam("achternaam");
+        assertEquals("achternaam", persoon.getAchternaam());
+    }
+
+    @Test
+    public void testSetTussenvoegsel() {
+        createPersoon();
+        persoon.setTussenvoegsel("tussenvoegsel");
+        assertEquals("tussenvoegsel", persoon.getTussenvoegsel());
+    }
+
+    @Test
+    public void testSetTelefoon() {
+        createPersoon();
+        persoon.setTelefoon("0612345678");
+        assertEquals("0612345678", persoon.getTelefoon());
+    }
+
+    @Test
+    public void testSetFax() {
+        createPersoon();
+        persoon.setFax("1234567890");
+        assertEquals("1234567890", persoon.getFax());
+    }
+
+    @Test
+    public void testSetGeslachtMan() {
+        createPersoon();
+        persoon.setGeslacht(Persoon.Geslacht.MAN);
+        assertEquals(Persoon.Geslacht.MAN, persoon.getGeslacht());
+    }
+
+    @Test
+    public void testToString() {
+        createPersoon();
+        assertEquals("Persoon [id=1, voornaam=Nick, tussenvoegsel=of, achternaam=Larie, telefoon=0612345678, fax=0612345678, geslacht=VROUW]",
+                persoon.toString());
     }
 
     private void createPersoon() {
